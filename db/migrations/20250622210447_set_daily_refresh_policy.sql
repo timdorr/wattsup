fcca84ccb463bf4688827633ad28017120681172
Tim Dorr <git@timdorr.com>
2025-08-26 17:16:30 -0400

    Capture more data and update more frequently

--- db/migrations/20250622210447_set_daily_refresh_policy.sql
+++ db/migrations/20250622210447_set_daily_refresh_policy.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 SELECT add_continuous_aggregate_policy('metrics_hourly',
-  start_offset => INTERVAL '1 hour',
+  start_offset => INTERVAL '2 hour',
   end_offset => NULL,
-  schedule_interval => INTERVAL '5m');
+  schedule_interval => INTERVAL '1m');
 
 -- migrate:down
 SELECT remove_continuous_aggregate_policy('metrics_hourly');
